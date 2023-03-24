3db560f832fd30bd301f321a37e6d580dfbc6f3c
kesantos <kesantos@paschoalotto.com.br>
2023-03-24 11:27:43 -0300

    feat(Monorepo): Created Monorepo

---
+++ node_modules/server/src/server.ts
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+
+const app = Fastify()
+
+app.register(cors)
+
+app.listen({
+  port: 3333,
+  host: '0.0.0.0',
+}).then(() => {
+  console.log('Server is running!')
+})
+
+app.get('/', () => {
+  return {
+    status: 'OK',
+    code: 200
+  }
+})
